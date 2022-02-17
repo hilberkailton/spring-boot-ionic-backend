f3e661f25289c99a0c7ab92173c915afa00b4707
unknown <hylber.kailton2014@gmail.com>
2022-02-17 15:19:42 -0300

    Listando todas categorias com DTO

--- src/main/java/com/hilber/cursomc/services/CategoriaService.java
+++ src/main/java/com/hilber/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.hilber.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,4 +44,9 @@ public class CategoriaService {
 
 	}
 
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	
 }
